7742a39babe1f2f039f5b7bce72e64ba05e2df33
Cindy Liang <hakkacindy@gmail.com>
2021-04-27 21:02:07 +0100

    done models

---
+++ app/models/bookmark.rb
@@ -0,0 +1,7 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  validates :movie, uniqueness: { scope: :list,
+    message: "you can't put the same movie twice!" }
+  validates :comment, length: { minimum: 6 }
+end
